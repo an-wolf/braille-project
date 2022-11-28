c3268622b577baf6d645dbbe10104ca7c021d190
an-wolf <110436808+an-wolf@users.noreply.github.com>
2022-11-28 16:49:52 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,65 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . # . . .
+        # # . . .
+        . # . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(2000)
+    basic.showLeds(`
+        # . . . .
+        . # . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    basic.showLeds(`
+        # # . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    basic.showLeds(`
+        . # . . .
+        . # . . .
+        # # . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(2000)
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    basic.showLeds(`
+        # # . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(3000)
+    basic.showLeds(`
+        . . . . .
+        # # . . .
+        . # . . .
+        . . . . .
+        . . . . .
+        `)
+})
